c20ce1193574838daa471afd1fed2d6ab4cf0bf3
Your Name <you@example.com>
2020-06-01 23:13:00 +0530

    initial commit

--- iot_security/config.py
+++ iot_security/config.py
@@ -4,18 +4,18 @@ from datetime import timedelta
 
 class Config:
     ENV = 'production'
-    SECRET_KEY = 'e0ad86e4a68d6aeef26cb571ea7a6524c3aebc825a452b375007dff823645e4f'
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres:password@localhost:5432/be_project"
+    SECRET_KEY = 'enter a secret key'
+    SQLALCHEMY_DATABASE_URI = "postgres://postgres:<password>@localhost:5432/"
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME' , 'authelectric@gmail.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD' , 'Moderncoe@123')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME' , '')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD' , '')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'authelectric@gmail.com')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', False)
     REMEMBER_COOKIE_DURATION = timedelta(hours=24)
-    ENCRYPT_KEY = 'ddc450603b1a0b804eb6bc3ec5a95f0e'
-    ENCRYPT_IV = 'u\xa4\x94@\x9b\x08\xa6I\xe1w!E\xcb\xed\x934'
+    ENCRYPT_KEY = ''
+    ENCRYPT_IV = ''
     # REMEMBER_COOKIE_PATH = 
     # REMEMBER_COOKIE_SECURE =
     # REMEMBER_COOKIE_HTTPONLY =
@@ -31,7 +31,7 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     ENV = 'development'
     DEBUG = False
-    SECRET_KEY = 'e0ad86e4a68d6aeef26cb571ea7a6524c3aebc825a452b375007dff823645e4f'
+    SECRET_KEY = 'enter a key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', Config.SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RATELIMIT_DEFAULT = '10/hour;100/day;2000 per year'
